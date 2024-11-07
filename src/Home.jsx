b764c48612c17d6a2e89b45abd19ac64144e4f45
pranmandale <pranavmandale2025@gmail.com>
2024-11-07 12:31:02 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1 className='text-center text-gray-600'>This is Home page of our website</h1>
+    </div>
+  )
+}
+
+export default Home
